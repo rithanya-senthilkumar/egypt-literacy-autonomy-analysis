4527b061cfefa603f5e42ca598272bebe960510b
Rithanya Senthilkumar <44210088-parotta4lyf@users.noreply.replit.com>
2025-08-07 16:29:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This code is written in python
+# The pandas library is used for data processing and to read data files
+import pandas as pd 
+#The matplotlib library is used to plot histograms and scatter plots
+import matplotlib.pyplot as plt
+# The GWCutilities has functions to help format data printed to the console
+import GWCutilities as util
+
+# Read a comma separated values (CSV) files into a variable
+# as a pandas DataFrame
+
+# If you wish change which data set you are working with, do that here: 
+lwd=pd.read_csv("livwell135.csv")
+
+# Add Step 1 code here:
+
+
+# 
